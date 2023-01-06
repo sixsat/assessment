f85fbb4824dd20cfa795c1c13b63caf9f21dc922
sat <77387934+sixsat@users.noreply.github.com>
2023-01-06 16:11:27 +0700

    Add unit test for getting all expenses

--- pkg/expense/handler_test.go
+++ pkg/expense/handler_test.go
@@ -91,6 +91,42 @@ func TestGetExpense(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
+func TestGetAllExpenses(t *testing.T) {
+	// Arrange
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	SetDB(db)
+
+	rows := mock.NewRows([]string{"id", "title", "amount", "note", "tags"}).
+		AddRow(1, "strawberry smoothie", 79.0, "night market promotion discount 10 bath", pq.Array([]string{"food", "beverage"}))
+	mock.ExpectPrepare(regexp.QuoteMeta("SELECT * FROM expenses")).
+		ExpectQuery().
+		WillReturnRows(rows)
+
+	req := httptest.NewRequest(http.MethodGet, "/expenses", strings.NewReader(""))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	e := echo.New()
+	c := e.NewContext(req, rec)
+
+	expected := `[{"id":1,"title":"strawberry smoothie","amount":79,"note":"night market promotion discount 10 bath","tags":["food","beverage"]}]
+`
+
+	// Act
+	err = GetAllExpenses(c)
+	actual := rec.Body.String()
+
+	// Assertions
+	assert.NoError(t, err)
+	assert.NoError(t, mock.ExpectationsWereMet())
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, expected, actual)
+}
+
 func TestUpdateExpense(t *testing.T) {
 	// Arrange
 	db, mock, err := sqlmock.New()
