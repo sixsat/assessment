fca43fb11e66fbef13de0fa596be6c0d79a03147
sat <77387934+sixsat@users.noreply.github.com>
2023-01-07 00:29:57 +0700

    Add integration test for getting an expense

--- pkg/expense/handler_it_test.go
+++ pkg/expense/handler_it_test.go
@@ -23,6 +23,11 @@ func TestITCreateExpense(t *testing.T) {
 	eh := setup(t)
 	defer teardown(t, eh)
 
+	_, err := db.Exec("TRUNCATE TABLE expenses RESTART IDENTITY")
+	if err != nil {
+		t.Log(err)
+	}
+
 	body := strings.NewReader(`
 		{"title": "strawberry smoothie", "amount": 79, "note": "night market promotion discount 10 bath", "tags": ["food","beverage"]}
 	`)
@@ -49,13 +54,36 @@ func TestITCreateExpense(t *testing.T) {
 	}
 }
 
+func TestITGetExpense(t *testing.T) {
+	// Arrange
+	eh := setup(t)
+	defer teardown(t, eh)
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/expenses/1", serverPort), strings.NewReader(""))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
+
+	expected := `{"id":1,"title":"strawberry smoothie","amount":79,"note":"night market promotion discount 10 bath","tags":["food","beverage"]}
+`
+
+	// Act
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+
+	byteBody, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, expected, string(byteBody))
+	}
+}
+
 func setup(t *testing.T) *echo.Echo {
 	InitDB("host=localhost port=5432 user=root password=secret dbname=expense sslmode=disable")
-
-	_, err := db.Exec("TRUNCATE TABLE expenses RESTART IDENTITY")
-	if err != nil {
-		t.Log(err)
-	}
 
 	eh := echo.New()
 	go func(e *echo.Echo) {
